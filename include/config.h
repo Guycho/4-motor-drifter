45d4ba4702237f7f293fc4477596f58aad858772
Guy Raiz <guy@verobotics.com>
2025-01-22 15:37:08 +0200

    better config layout

--- include/config.h
+++ include/config.h
@@ -7,19 +7,21 @@ enum WheelsPositions { FR = 0, RR = 1, RL = 2, FL = 3 };
 enum SteeringPositions { R = 0, L = 1 };
 
 namespace Config {
-const uint8_t num_wheels = 4;
-const uint8_t num_steering = 2;
-const float min_percentage = -100.0;
-const float max_percentage = 100.0;
-const float max_omega = 360; // deg/s
-const float steering_r_l_ratio = 0.1;
-const float trim_increment = 0.01;
-const float cs_ratio = 0.7;
-const uint16_t hb_timeout = 250;
-const uint8_t arm_led_pin = 16;
+extern const uint8_t num_wheels;
+extern const uint8_t num_steering;
+extern const float min_percentage;
+extern const float max_percentage;
+extern const float max_omega;
+extern const float steering_r_l_ratio;
+extern const float trim_increment;
+extern const float cs_ratio;
+extern const uint16_t hb_timeout;
+extern const uint8_t arm_led_pin;
+
 namespace Transceiver {
 extern const uint16_t update_delay_ms;
-}  // namespace Transceiver
+}
+
 namespace Motor {
 extern const float deadband;
 namespace Wheel {
@@ -33,6 +35,7 @@ extern const uint16_t min_pulse[];
 extern const uint16_t max_pulse[];
 }  // namespace Steering
 }  // namespace Motor
+
 namespace PIDController {
 extern const float kp;
 extern const float ki;
@@ -43,10 +46,7 @@ extern const float low_pass_alpha;
 extern const float high_Pass_alpha;
 extern const bool use_filters;
 }  // namespace PIDController
-namespace PController {
-extern const float kp;
-extern const float max_output;
-}  // namespace PController
+
 namespace MavlinkBridge {
 extern HardwareSerial *serial;
 extern const uint32_t baudrate;
@@ -55,10 +55,12 @@ extern const uint8_t component_id;
 extern const uint16_t message_rate;
 extern const uint32_t is_alive_timeout;
 }  // namespace MavlinkBridge
+
 namespace ESPNow {
 extern const uint8_t peer_mac_address[];
 extern const bool use_lr;
 extern const bool print_debug;
 }  // namespace ESPNow
 }  // namespace Config
+
 #endif  // CONFIG_H
--- src/config.cpp
+++ src/config.cpp
@@ -1,9 +1,21 @@
 #include "config.h"
 
 namespace Config {
+const uint8_t num_wheels = 4;
+const uint8_t num_steering = 2;
+const float min_percentage = -100.0;
+const float max_percentage = 100.0;
+const float max_omega = 360;  // deg/s
+const float steering_r_l_ratio = 0.1;
+const float trim_increment = 0.01;
+const float cs_ratio = 0.7;
+const uint16_t hb_timeout = 250;
+const uint8_t arm_led_pin = 16;  // change to 2 on updated HW
+
 namespace Transceiver {
 const uint16_t update_delay_ms = 25;
-}  // namespace Server
+}
+
 namespace Motor {
 const float deadband = 1.0;
 namespace Wheel {
@@ -17,16 +29,18 @@ const uint16_t min_pulse[num_steering] = {1000, 1000};
 const uint16_t max_pulse[num_steering] = {2000, 2000};
 }  // namespace Steering
 }  // namespace Motor
+
 namespace PIDController {
-extern const float kp = 1;
-extern const float ki = 0.1;
-extern const float kd = 0.01;
-extern const float max_output = 100;
-extern const float integral_percentage = 30;
-extern const float low_pass_alpha = 0.1;
-extern const float high_Pass_alpha = 0.1;
-extern const bool use_filters = true;
+const float kp = 1;
+const float ki = 0.1;
+const float kd = 0.01;
+const float max_output = 100;
+const float integral_percentage = 30;
+const float low_pass_alpha = 0.1;
+const float high_Pass_alpha = 0.1;
+const bool use_filters = true;
 }  // namespace PIDController
+
 namespace MavlinkBridge {
 HardwareSerial *serial = &Serial2;
 const uint32_t baudrate = 2000000;
@@ -35,6 +49,7 @@ const uint8_t component_id = 0;
 const uint16_t message_rate = 100;
 const uint32_t is_alive_timeout = 2500;
 }  // namespace MavlinkBridge
+
 namespace ESPNow {
 const uint8_t peer_mac_address[] = {0xA0, 0xDD, 0x6C, 0x03, 0x9E, 0x40};
 const bool use_lr = true;
