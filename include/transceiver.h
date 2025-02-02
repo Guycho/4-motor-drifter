32fa2b6dc8ee2e375ccaa98a123d37e58b23b06f
Guy Raiz <guy@verobotics.com>
2025-02-02 11:38:35 +0200

    move data types to utils

--- include/transceiver.h
+++ include/transceiver.h
@@ -4,50 +4,19 @@
 #include <ArduinoJson.h>
 #include <Chrono.h>
 #include <WiFi.h>
-
 #include <bitset>
 
-#include "ESP_now_handler.h"
+#include <ESP_now_handler.h>
+#include <data_types.h>
 #include "config.h"
 
-struct RemoteControllerData {
-    float throttle = 0;
-    float steering = 0;
-    bool left_arrow = 0;
-    bool right_arrow = 0;
-    bool up_arrow = 0;
-    bool down_arrow = 0;
-    bool sel = 0;
-    bool ch = 0;
-    bool plus = 0;
-    bool minus = 0;
-    bool left_trim_l = 0;
-    bool left_trim_r = 0;
-    bool right_trim_l = 0;
-    bool right_trim_r = 0;
-    bool edge_switch = 0;
-    bool bottom_switch = 0;
-    bool new_data = 0;
-};
-
-struct TelemetryData {
-    bool arm_state;
-    uint8_t steering_mode;
-    uint8_t drive_mode;
-    uint8_t battery_status;
-    uint16_t motors_rpm[Config::num_wheels];
-    float battery_voltage;
-    float g_force_x;
-    float g_force_y;
-    float rotation_rate_z;
-    float motors_throttle[Config::num_wheels];
-    float steering_values[Config::num_steering];
-};
 
 struct TransceiverConfig {
     uint16_t update_delay_ms;
     ESPNowHandler *esp_now_handler;
 };
+
+using namespace DataTypes;
 
 class Transceiver {
    public:
@@ -72,4 +41,4 @@ class Transceiver {
     uint16_t m_update_delay_ms;
 };
 
-#endif  // ESP32SERVER_HPP
+#endif  // TRANSCEIVER_H
