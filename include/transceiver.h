7be2c34e78ba27448b5ad88eeea2e622c2869d96
Guy Raiz <guy@verobotics.com>
2025-02-02 12:24:41 +0200

    better transciever

--- include/transceiver.h
+++ include/transceiver.h
@@ -3,13 +3,14 @@
 
 #include <ArduinoJson.h>
 #include <Chrono.h>
+#include <ESP_now_handler.h>
 #include <WiFi.h>
+#include <data_types.h>
+#include <utils.h>
+
 #include <bitset>
 
-#include <ESP_now_handler.h>
-#include <data_types.h>
 #include "config.h"
-
 
 struct TransceiverConfig {
     uint16_t update_delay_ms;
@@ -30,7 +31,6 @@ class Transceiver {
 
    private:
     void send_data();
-    bool verify_checksum(const String &data);
     RemoteControllerData parse_remote_data(const String &data);
     Chrono m_data_timer;
     ESPNowHandler *m_esp_now_handler;
--- src/transceiver.cpp
+++ src/transceiver.cpp
@@ -15,7 +15,7 @@ void Transceiver::run() {
         return;
     }
     m_remote_data = m_esp_now_handler->get_data();
-    if (!(m_remote_data.length() == 0 || !verify_checksum(m_remote_data))) {
+    if (m_remote_data.length() != 0 && Calcs::verify_checksum(m_remote_data)) {
         m_remote_controller_data = parse_remote_data(m_remote_data);
     }
     send_data();
@@ -29,53 +29,19 @@ RemoteControllerData Transceiver::get_remote_data() {
 
 void Transceiver::set_telemetry_data(const TelemetryData &data) { m_telemetry_data = data; }
 
-bool Transceiver::verify_checksum(const String &data) {
-    JsonDocument m_json_data;
-    deserializeJson(m_json_data, data);
-    // Extract the checksum from the JSON document
-    uint8_t received_checksum = m_json_data["c"];
-    // Remove the checksum from the JSON document
-    m_json_data.remove("c");
-    String json;
-    serializeJson(m_json_data, json);
-    
-    uint8_t calculated_checksum = Calcs::calc_checksum(json);
-    if (received_checksum != calculated_checksum) {
-        return false;  // Checksum mismatch;
-    }
-    return true;
-}
-
 RemoteControllerData Transceiver::parse_remote_data(const String &data) {
     JsonDocument m_json_data;
     deserializeJson(m_json_data, data);
     // Extract the bitmask from the JSON document
-    uint32_t bitmask = m_json_data["b"];
+    uint32_t bitmask_value = m_json_data["b"];
     RemoteControllerData remote_data;
-    // Unpack the bitmask
-    remote_data.throttle = (((bitmask >> 0) & 0x1FF) / 2.555) - 100;  // 9 bits for throttle
-    remote_data.steering = (((bitmask >> 9) & 0x1FF) / 2.555) - 100;  // 9 bits for steering
-    // compensate for resolution loss in the remote
-    if (abs(remote_data.throttle) < 0.3) {
-        remote_data.throttle = 0;
+    const uint8_t bitmask_size = sizeof(typeof(remote_data));
+    std::bitset<bitmask_size> bitmask(bitmask_value);
+    uint8_t *data_ptr = reinterpret_cast<uint8_t *>(&remote_data);
+    for (size_t i = 0; i < sizeof(TelemetryData); ++i) {
+        std::bitset<8> byte((bitmask >> (i * 8)).to_ulong() & 0xFF);
+        data_ptr[i] = static_cast<uint8_t>(byte.to_ulong());
     }
-    if (abs(remote_data.steering) < 0.3) {
-        remote_data.steering = 0;
-    }
-    remote_data.left_arrow = (bitmask >> 18) & 0x1;
-    remote_data.right_arrow = (bitmask >> 19) & 0x1;
-    remote_data.up_arrow = (bitmask >> 20) & 0x1;
-    remote_data.down_arrow = (bitmask >> 21) & 0x1;
-    remote_data.sel = (bitmask >> 22) & 0x1;
-    remote_data.ch = (bitmask >> 23) & 0x1;
-    remote_data.plus = (bitmask >> 24) & 0x1;
-    remote_data.minus = (bitmask >> 25) & 0x1;
-    remote_data.left_trim_l = (bitmask >> 26) & 0x1;
-    remote_data.left_trim_r = (bitmask >> 27) & 0x1;
-    remote_data.right_trim_l = (bitmask >> 28) & 0x1;
-    remote_data.right_trim_r = (bitmask >> 29) & 0x1;
-    remote_data.edge_switch = (bitmask >> 30) & 0x1;
-    remote_data.bottom_switch = (bitmask >> 31) & 0x1;
     remote_data.new_data = true;
     return remote_data;
 }
@@ -85,8 +51,6 @@ void Transceiver::send_data() {
     String json;
 
     const uint8_t bitmask_size = sizeof(typeof(m_telemetry_data));
-    m_json_data["s"] = bitmask_size;
-
     std::bitset<bitmask_size> bitmask;
 
     uint8_t *data_ptr = reinterpret_cast<uint8_t *>(&m_telemetry_data);
